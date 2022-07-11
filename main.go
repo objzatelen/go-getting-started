9f1e211148ef6351fb73e858d6d2657aff58e825
e210976 <oleksandr.pylypenko@tietoevry.com>
2022-07-11 22:45:30 +0200

    Articles added

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -9,6 +11,15 @@ import (
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
+type Article struct {
+	Id      string `json:"Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
+
 func main() {
 	port := os.Getenv("PORT")
 
@@ -16,14 +27,30 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
+	Articles = []Article{
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Title: "enclave", Desc: "An enclave is an area inhabited by people who are different in some way from the people in the surrounding areas.", Content: "Article Content"},
+		Article{Title: "🃏Jovial", Desc: "jovial means \"markedly good-humored\" and describes people and things that are cheerful or full of joy.", Content: "Article Content"},
+	}
+
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
+	router.GET("/articles", getAlbums)
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
 	router.Run(":" + port)
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, Articles)
 }
